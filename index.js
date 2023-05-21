d897e47004e062d138f92791b2e70156efe16420
Holedesu <88774183+Holedesu@users.noreply.github.com>
2023-05-21 15:39:38 +0300

    Added formatting to code and add .gitignore

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ import _ from 'lodash';
 
 export default function solution(content){
   // BEGIN
-  console.log(content);
+
+  const graph_split = '\r\n';
+  const formatted_content = content.split(graph_split);
+  const info_column = [];
+  for (let element in formatted_content) {
+    const column_split = ','
+    const formatted_column = formatted_content[element].split(column_split)
+    info_column.push(formatted_column)
+
+  }
+
+  console.log(info_column)
+
   // END
 }
